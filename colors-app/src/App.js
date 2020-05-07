15e0d2837994d827639f7d9d4fc1aaae83d0b837
David Hobson <davidhobson96@gmail.com>
2020-05-07 14:44:32 -0400

    Create route to render palette based on url id param.

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -5,16 +5,17 @@ import seedColors from "./seedColors";
 import { generatePalette } from './colorHelpers';
 
 class App extends Component{
+  
+  findPalette(id) {
+    return seedColors.find(p => p.id === id);
+  }
+
   render() {
     return (
       <Switch>
         <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE!</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>INDIVIDUAL PALETTE!</h1>}/>
+        <Route exact path="/palette/:id" render={routeProps => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/> }/>
       </Switch>
-      
-      // <div className="App">
-      //   <Palette palette={generatePalette(seedColors[4])}/>
-      // </div>
     );
   }
 }
