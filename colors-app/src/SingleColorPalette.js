298bd0ff8d087e24c5b20e0cf1b4b444b360b4c5
David Hobson <davidhobson96@gmail.com>
2020-05-08 15:37:04 -0400

    Add PaletteFooter and add format change functionality.

--- colors-app/src/SingleColorPalette.js
+++ colors-app/src/SingleColorPalette.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 
 class SingleColorPalette extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            format: "hex"
+        }
+
         this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+
+        this.changeFormat = this.changeFormat.bind(this);
     }
 
     gatherShades(palette, colorToFilterBy) {
@@ -18,17 +26,27 @@ class SingleColorPalette extends Component {
         return shades.slice(1);
     }
 
+    changeFormat(val){
+        this.setState({
+            format: val
+        });
+    }
+
     render() {
+        const { format } = this.state;
+        const { palette } = this.props;
+
         const colorBoxes = this._shades.map(c => (
-            <ColorBox key={c.id} name={c.name} background={c.hex} showLink={false}/>
+            <ColorBox key={c.id} name={c.name} background={c[format]} showLink={false}/>
         ));
 
         return (
             <div className="Palette">
-                <h1>SINGLE COLOR!</h1>
+                <Navbar handleChange={this.changeFormat} showingAllColors={false}/>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
+                <PaletteFooter paletteName={palette.paletteName} emoji={palette.emoji}/>
             </div>
         )
     }
