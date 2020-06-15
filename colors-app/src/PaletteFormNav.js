ebdd8972438545db5a9dae7f0bc375f7f947bd46
David Hobson <davidhobson96@gmail.com>
2020-06-15 16:10:34 -0400

    Create styles for new palette navbar.

--- colors-app/src/PaletteFormNav.js
+++ colors-app/src/PaletteFormNav.js
@@ -9,6 +9,39 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 400;
+
+const styles = theme => ({
+    root: {
+        display: "flex"
+    },
+    appBar: {
+        transition: theme.transitions.create(["margin", "width"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+        }),
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "64px"
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(["margin", "width"], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 20
+    },
+    navBtns: {
+
+    }
+});
 
 class PaletteFormNav extends Component {
     constructor(props) {
@@ -38,15 +71,15 @@ class PaletteFormNav extends Component {
         const { newPaletteName } = this.state;
 
         return (
-            <div>
+            <div className={classes.root}>
                 <CssBaseline />
-                <AppBar
-                    color="default"
-                    position='fixed'
-                    className={classNames(classes.appBar, {
-                        [classes.appBarShift]: open
-                    })}
-                >
+            <AppBar
+                color="default"
+                position='fixed'
+                className={classNames(classes.appBar, {
+                    [classes.appBarShift]: open
+                })}
+            >
                     <Toolbar disableGutters={!open}>
                         <IconButton
                             color='inherit'
@@ -56,19 +89,21 @@ class PaletteFormNav extends Component {
                         >
                             <MenuIcon />
                         </IconButton>
-                        <Typography variant='h6' color='inherit' noWrap>Persistent drawer</Typography>
+                        <Typography variant='h6' color='inherit' noWrap>Create a Palette</Typography>
+                    </Toolbar>
+                    <div className={classes.navBtns}>
                         <ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
                             <TextValidator label="Palette Name" name="newPaletteName" value={this.state.newPaletteName} onChange={this.handleChange} validators={["required", "isPaletteNameUnique"]} errorMessages={["Enter a palette name.", "Palette name must be unique."]}/>
                             <Button variant="contained" color="primary" type="submit">Save Palette</Button>
-                            <Link to="/">
-                                <Button variant="contained" color="secondary">Go Back</Button>
-                            </Link>
                         </ValidatorForm>
-                    </Toolbar>
+                        <Link to="/">
+                                <Button variant="contained" color="secondary">Go Back</Button>
+                        </Link>
+                    </div>
                 </AppBar>
             </div>
         )
     }
 }
 
-export default PaletteFormNav;
+export default withStyles(styles, { withTheme: true })(PaletteFormNav);
