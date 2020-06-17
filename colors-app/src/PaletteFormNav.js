2c15373e8d5ff25121e18f309327596c79b86de3
David Hobson <davidhobson96@gmail.com>
2020-06-17 11:52:04 -0400

    Update icon for drawer.

--- colors-app/src/PaletteFormNav.js
+++ colors-app/src/PaletteFormNav.js
@@ -5,7 +5,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import PaletteRoundedIcon from '@material-ui/icons/PaletteRounded';
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
@@ -60,9 +61,9 @@ class PaletteFormNav extends Component {
                             color='inherit'
                             aria-label='Open drawer'
                             onClick={this.props.handleDrawerOpen}
-                            className={classNames(classes.menuButton, open && classes.hide)}
+                            className={classNames(classes.menuButton, {[classes.hide]: open})}
                         >
-                            <MenuIcon />
+                            <PaletteRoundedIcon />
                         </IconButton>
                         <Typography variant='h6' color='inherit' noWrap>Create a Palette</Typography>
                     </Toolbar>
