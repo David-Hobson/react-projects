5780d14176521703f71771b4f813fe7051632cef
David Hobson <davidhobson96@gmail.com>
2020-06-17 23:20:14 -0400

    Add classnames to ColorBox copying.

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from 'react-router-dom';
+import classNames from "classnames";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/ColorBoxStyles";
 
@@ -28,8 +29,8 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className={classes.ColorBox} style={{ background }}>
-                    <div className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} style={{ background }} />
-                    <div className={`${classes.copyMessage} ${copied && classes.showMessage}`} >
+                    <div className={classNames(classes.copyOverlay, {[classes.showOverlay]: copied})} style={{ background }} />
+                    <div className={classNames(classes.copyMessage, {[classes.showMessage]: copied})} >
                         <h1>Copied!</h1>
                         <p className={classes.copyText}>{background}</p>
                     </div>
