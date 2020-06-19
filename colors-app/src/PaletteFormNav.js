3d969aee9a4c3039eaca904acdaff9972997a029
David Hobson <davidhobson96@gmail.com>
2020-06-18 20:26:42 -0400

    Fix material UI style duplicated overrides.

--- colors-app/src/PaletteFormNav.js
+++ colors-app/src/PaletteFormNav.js
@@ -8,7 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 import PaletteRoundedIcon from '@material-ui/icons/PaletteRounded';
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/styles";
 import styles from "./styles/PaletteFormNavStyles";
 
 import PaletteMetaForm from "./PaletteMetaForm";
